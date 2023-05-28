5ac8aef0655bedc8f7ea0cdaaeecf6a36b06f541
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2023-05-28 10:53:50 -0400

    read.go, write.go files

---
+++ read.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func read() {
+	fmt.Println("Grand terminal app...")
+}
---
+++ write.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"time"
+)
+
+func write(key string) {
+
+	t := time.Now()
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+
+	fpath := path.Join(home, ".notes", key)
+
+	if _, err := os.Stat(fpath); os.IsNotExist(err) {
+		os.MkdirAll(fpath, 0700)
+	}
+
+	fname := filepath.Join(fpath, fmt.Sprintf("%s.md", t.Format("20060102150405")))
+
+	f, err := os.Create(fname)
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+
+	defer f.Close()
+
+	e := os.Getenv("EDITOR")
+
+	if e == "" {
+		e = "vim"
+	}
+
+	cmd := exec.Command(e, fname)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	err = cmd.Run()
+
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+
+	fmt.Printf("Created: %s\n", fname)
+}
